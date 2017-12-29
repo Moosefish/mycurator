995150e5cbae4bdb4c34344cdc725b390a24923b
John Nemec <johnnemec@gmail.com>
2017-12-28 23:34:28 -0600

    Initial design

---
+++ design/index.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>My Curator</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Moosefish Web Services, LLC">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css"> -->
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+  </head>
+  <body>
+
+    <!-- Navigation -->
+    <div class="brand" style="text-align:center">
+        <a href="#header"><img src="/img/logo.png" alt="My Curator Logo"></a>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mr-auto ml-5">
+            <li class="nav-item mr-5">
+              <a class="nav-link text-white" href="#">How it works</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">About</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto mr-5">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Contact</a>
+            </li>
+            <li class="nav-item ml-5">
+              <a class="nav-link text-white" href="#">Log In</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container hero-banner">
+      <div class="row text-center pb-0 mb-0 align-items-center">
+        <div class="col">
+          <p class="hero-lead">What's on your mind, in your inbox.</p>
+          <form>
+            <div class="form-row justify-content-center">
+              <div class="col-4">
+                <input type="text" class="form-control form-control-lg box-shadow-dark" id="querysearch" placeholder="What are you looking for?">
+                <button type="submit" class="btn btn-primary btn-sm mt-4 text-uppercase">Tell us to get lookin'</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container" id="main">
+      <div class="row">
+        
+        <section>
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-lg-6 order-lg-1">
+                <div class="pt-3 pl-5">
+                  <img class="img-fluid box-shadow" src="http://via.placeholder.com/500x550" alt="">
+                </div>
+              </div>
+              <div class="col-lg-6 order-lg-2">
+                <div class="p-5 bg-grey box-shadow callbox-left">
+                    <p>The funny thing about choice&mdash;it's freeing. But if there's too much of it, it becomes a prison. Because you need to find <strong>My Curator</strong> and you need to make a (wait for it)... <a href="#"><i>good decision</i></a>.</p>
+                </div>
+                <div class="row justify-content-center">
+                  <div class="col-8">
+                    <p class="txt-pink pt-5">That's pressure. That time-consuming. And in this day and age, that's just not cool.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="up-15">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-lg-6 order-lg-2">
+                <div class="pb-5">
+                  <img class="img-fluid box-shadow" src="http://via.placeholder.com/500x350" alt="">
+                </div>
+              </div>
+              <div class="col-lg-6 order-lg-1">
+                <div class="p-5 bg-pink box-shadow callbox-right">
+                  <p><strong>Stop comparing and start <a href="#">choosing</a> with My Curator.</strong></p>
+                  <p>Every day, we'll curate 5 options that fit the description of whatever's on your list and send them straight to your inbox.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-6 offset-2">
+                <p>Say no to them all. No biggie. Because tomorrow, we'll hit you up with 5 fresh options. Stop the emails when you've found something, or when you just lose interest. No matter what happens, your day has been made a little easier.</p>
+                <p class="txt-pink text-center">Go show online shopping who's boss.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="pt-5">
+      <div class="container">
+        
+        <div class="row justify-content-center">
+          <div class="col-10 text-white text-center">
+            <h3 class="mb-4">Easy step-by-step</h3>
+            <div class="row">
+              <div class="col">
+                <p class="small">Step 1</p>
+                Icon Image
+                <p class="small">Visit MyCuratorGo.com</p>
+              </div>
+              <div class="col">
+                <p class="small">Step 2</p>
+                Icon Image
+                <p class="small">Enter the item you're looking for</p>
+              </div>
+              <div class="col">
+                <p class="small">Step 3</p>
+                Icon Image
+                <p class="small">Go about your business</p>
+              </div>
+              <div class="col">
+                <p class="small">Step 4</p>
+                Icon Image
+                <p class="small">Receive your email with top 5 items</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-10 bg-darkblue">
+              <div class="row justify-content-center">
+              <form class="col-4 py-5">
+                <p class="text-white text-center text-uppercase">Sign Up Now!</p>
+                <div class="input-group">
+                  <input type="email" class="form-control rounded-0" id="footerEmail" aria-describedby="footerEmailHelp" placeholder="Enter Your Email Address...">
+                  <div class="input-group-append">
+                    <button class="btn btn-primary rounded-0" type="button"><span class="ion-ios-arrow-right"></span></button>
+                  </div>
+                </div>
+              </form>
+              </div>
+              <div class="row justify-content-center pb-5 text-center">
+                <div class="col-1">
+                    <p class="h1"><a href="#"><span class="ion-social-twitter text-white"></span></a></p>
+                </div>
+                <div class="col-1">
+                  <p class="h1"><a href="#"><span class="ion-social-facebook text-white"></span></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
